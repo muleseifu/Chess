16ddec8b6a5eed89610947a21e0b54295a10c5dc
Muluken Seifu <mulerseifu87@gmail.com>
2026-05-15 14:16:45 +0300

    add methods

--- gui/MainMenuPanel.java
+++ gui/MainMenuPanel.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.event.*;
 
 
-public class MainMenuPanel {
+public class MainMenuPanel extends JLabel{
     private JButton btnPvP;
     private JButton btnPVB;
     private JButton btnSettings;
@@ -12,6 +12,23 @@ public class MainMenuPanel {
     private JComboBox difficultyBox;
 
 
+    public void intUI(){
+
+    }
+
+    public void onPvPClicked(){
+
+    }
+
+    public void onPVBClicked(){
+
+    }
+
+    public void onSettingsClicked(){
+
+    }
     
-    
+    public void setDifficultyVisible(boolean b){
+        
+    }
 }
