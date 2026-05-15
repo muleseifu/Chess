e4c61f7027a18e41e0031859e8a716b8e8d1580d
Abere <aberechanie@gmail.com>
2026-05-15 10:51:10 +0300

    Update Piece class

--- model/pieces/Piece.java
+++ model/pieces/Piece.java
@@ -1,5 +1,54 @@
 package Chess.model.pieces;
 
-public class Piece {
+public abstract  class Piece {
+    protected int color;
+    protected String id ;
+    protected String imagePath;
+    protected boolean hasMoved;
+    protected boolean availability;
+    public void SetId(String id){
+        this.id = id;
+    }
+    public void SetColor(int color) {
+        this.color = color;
+    }
+    public void SetPath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+    public void setAvailable(boolean availability) {
+        this.availability = availability;
+    }
+     public void setMoved() {
+        this.hasMoved = true;
+    }
+    public int getColor() {
+        return color;
+    }
+    public String getId() {
+        return id;
+    }
+    public String getID()
+    {
+        return id;
+    }
+    
+    public String getPath() {
+        return imagePath;
+    }
+    public boolean isAvailable() {
+        return availability;
+    }
+    public boolean hasMoved() {
+        return hasMoved;
+    }
+  public void test() {
+        System.out.println("This is a test method in the Piece class.");
+    }
+   
+
+
+
+
+
     
 }
