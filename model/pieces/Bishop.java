317e83764b28103f32e7d23e9fe76e9494d2f18c
Psuedo-Mate17 <tinalomitull@gmail.com>
2026-05-20 10:26:21 +0300

    @override

--- model/pieces/Bishop.java
+++ model/pieces/Bishop.java
@@ -8,9 +8,11 @@ public class Bishop extends Piece {
     public Bishop(int color, String id, String imagePath) {
         super(color, id, imagePath);
     }
+    @override
     public List<Cell> move(Cell pos,Board board){
         return;
     }
+    @override
     public Piece getCopy(){
         return;
     }
